89318dc98e31ad7e574e074440cfe163d916104c
M <M@DESKTOP-K0HAN3Q>
2020-06-20 12:52:04 +0200

    Add project files.

---
+++ RandsomNoteWriter/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RandsomNoteWriter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.RichTextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox.Location = new System.Drawing.Point(12, 12);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(695, 454);
+            this.TextBox.TabIndex = 0;
+            this.TextBox.Text = "";
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(713, 12);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(75, 23);
+            this.buttonConvert.TabIndex = 1;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.ConvertClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 478);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.TextBox);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextBox;
+        private System.Windows.Forms.Button buttonConvert;
+    }
+}
+
---
+++ RandsomNoteWriter/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Windows.Forms;
+
+namespace RandsomNoteWriter
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        Random random = new Random();
+        
+        
+
+        public List<string> GetFontFamilies()
+        {
+            List<string> fontfamilies = new List<string>();
+            foreach (FontFamily family in FontFamily.Families)
+            {
+                fontfamilies.Add(family.Name);
+            }
+            return fontfamilies;
+
+        }
+
+
+        public int RandomNumber(int min, int max)
+        {
+
+            return random.Next(min, max);
+        }
+
+        private void ConvertClick(object sender, EventArgs e)
+        {
+            List<string> font_list = GetFontFamilies();
+                
+            for (int i = 0; i < (TextBox.Text.Length - 1); i++)
+            {
+                TextBox.SelectionStart = i;
+                TextBox.SelectionLength = 1;
+                TextBox.SelectionFont = new System.Drawing.Font(font_list[RandomNumber(0, (font_list.Count - 1))], 14);
+
+
+            }
+        }
+    }
+
+}
